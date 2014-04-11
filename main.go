eb65719431cdbd9614eb173f544c9eec0a145ecf
Bertrand Janin <b@janin.com>
2014-04-11 19:51:21 -0400

    Document the flow of data in a shitty ASCII diagram.

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 // Copyright 2014, Truveris Inc. All Rights Reserved.
 // Use of this source code is governed by the ISC license in the LICENSE file.
+//
+// Here is a shitty diagram to explain how this work.
+//
+//           +------------------+       +-----------------+
+//           | IRC Socket Write |       | IRC Socket Read |
+//           +----------------_-+       +-_---------------+
+//                            |           |
+//              /|\     +-----^---+   +---^----+       |
+//               |      | irc out |   | irc in |       |
+//               |      +---------+   +--------+       |
+//               |                 \ /                 |
+//               |                  |                  |
+//        to irc |         +--------^--------+         | from irc
+//               |         | sqs-irc-gateway |         |
+//               |         +--------_--------+         |
+//               |                  |                  |
+//               |                 / \                 |
+//               |       +--------+   +---------+      |
+//               |       | sqs in |   | sqs out |     \|/
+//                       +----_---+   +---_-----+
+//                            |           |
+//         +------------------^-+       +-^------------------+
+//         | IRC Outgoing Queue |       | IRC Incoming Queue |
+//         +--------------------+       +--------------------+
+//
+// The program using these queues just need to read the incoming queue and
+// write to the outgoing queue.
+//
 
 package main
 
@@ -34,12 +62,14 @@ func start() error {
 	go connectionWriter(conn, ircout)
 
 	// These channels represent the lines coming and going to the SQS
-	// queues (from the perspective of this program).
-	sqsin, sqsinerrch, err := sqschan.Incoming(client, cfg.IncomingQueueName)
+	// queues (from the perspective of this program). The incoming channel
+	// for SQS is hooked up to the outgoing IRC queue (this process receives
+	// all the IRC messages going back to the server).
+	sqsin, sqsinerrch, err := sqschan.Incoming(client, cfg.OutgoingQueueName)
 	if err != nil {
 		return err
 	}
-	sqsout, sqsouterrch, err := sqschan.Outgoing(client, cfg.OutgoingQueueName)
+	sqsout, sqsouterrch, err := sqschan.Outgoing(client, cfg.IncomingQueueName)
 	if err != nil {
 		return err
 	}
